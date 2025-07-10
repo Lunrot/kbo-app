f4eb5dc531e3f5ec92e419df0ac586c6869391f8
Lunrot <lunroth@gmail.com>
2025-07-10 15:46:43 +0900

    Update web.py

--- web.py
+++ web.py
@@ -159,4 +159,5 @@ def update_graph(frame, selected):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8050))
+    app.run_server(host='0.0.0.0', port=port)
